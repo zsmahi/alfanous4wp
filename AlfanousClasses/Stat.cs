6531c75761092f77f663df8a496a2423fbfce4ec
AbdouMoumen <devnull@localhost>
2011-12-03 17:41:44 +0100

    added Sura's details

--- AlfanousClasses/Stat.cs
+++ AlfanousClasses/Stat.cs
@@ -1,24 +1,35 @@
 ﻿using System;
+
 namespace AlfanousWP7.AlfanousClasses
 {
     public class Stat
     {
+        protected string start = "الآية تحتوي على";
         public int? Letters { get; set; }
         public int? GodNames { get; set; }
         public int? Words { get; set; }
+
         public override string ToString()
         {
-            var kalimat = Words == 1
-                       ? "كلمة واحدة"
-                       : Words == 2
-                             ? "كلمتين"
-                             : Words >= 3 && Words <= 10
-                                   ? Words + " كلمات "
-                                   : Words + " كلمة ";
+            var single = "كلمة";
+            var couple = "كلمتين";
+            var plural = "كلمات";
+            string kalimat = NumberToString(Words.Value, single, couple, plural);
             var asmaaAllah = GodNames == 0
                                  ? string.Empty
                                  : string.Format("و {0} من أسماء الله الحسنى", GodNames);
-            return String.Format("الآية تحتوي على {0} حرفا، {1} {2}", Letters, kalimat, asmaaAllah);
+            return String.Format("{3} {0} حرفا، {1}  {2}", Letters, kalimat, asmaaAllah, start);
+        }
+
+        public static string NumberToString(int number, string single, string couple, string plural)
+        {
+            return number == 1
+                       ? String.Format("{0} واحدة", single)
+                       : number == 2
+                             ? couple
+                             : number >= 3 && number <= 10
+                                   ? String.Format("{0} {1}", number, plural)
+                                   : String.Format("{0} {1}", number, single);
         }
     }
 }
--- AlfanousClasses/SuraStat.cs
+++ AlfanousClasses/SuraStat.cs
@@ -3,5 +3,10 @@ namespace AlfanousWP7.AlfanousClasses
     public class SuraStat:Stat
     {
         public int? Ayas { get; set; }
+        public override string ToString()
+        {
+            start = string.Format("ÇáÓæÑÉ ÊÍæí {0}¡ ", NumberToString(Ayas.Value, "ÂíÉ", "ÂíÊíä", "ÂíÇÊ"));
+            return base.ToString();
+        }
     }
 }
